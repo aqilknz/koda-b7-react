8c1ff1b0e4a368fa4305560d4416534f6e5e789c
aqilknz <khairunnz123@gmail.com>
2026-04-02 22:01:56 +0700

    feat: add Navigate Feature in Header

---
+++ src/components/minitask4/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink } from "react-router"
+
+function Nav() {
+    return (
+        <nav className='w-full sticky top-0'>
+            <header className='sticky w-full bg-gray-800 text-white p-4'>
+                <ul className='flex gap-10 justify-center items-center text-xl font-medium'>
+                    <li ><NavLink to="/" className={({isActive}) => {
+                        return isActive ? "text-pink-600" : "text-white hover:text-pink-600"
+                    }}>Home</NavLink></li>
+                    <li ><NavLink to="/pokemon" className={({isActive}) => {
+                        return isActive ? "text-pink-600" : "text-white hover:text-pink-600"
+                    }}>Pokemon List</NavLink></li>
+                    <li><NavLink to="/app" className={({isActive}) => {
+                        return isActive ? "text-pink-600" : "text-white hover:text-pink-600"
+                    }}>Form Product</NavLink></li>
+                    <li><NavLink to="/count" className={({isActive}) => {
+                        return isActive ? "text-pink-600" : "text-white hover:text-pink-600"
+                    }}>Count</NavLink></li>
+                </ul>
+            </header>
+        </nav>
+    )
+}
+
+export default Nav
