30c677ee67ced6d554fbe4ba38e2da3169ed95b4
aqilknz <khairunnz123@gmail.com>
2026-04-02 14:59:43 +0700

    feat: add install react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import Count from './components/minitask1/count.jsx';
 import App from './components/minitask2/App.jsx';
@@ -7,8 +8,12 @@ import FetchPokemon from './components/minitask3/fetchPokemon.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Count />
-    <App />
-    <FetchPokemon />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pokemon" element={<FetchPokemon />} />
+        <Route path="/count" element={<Count />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
