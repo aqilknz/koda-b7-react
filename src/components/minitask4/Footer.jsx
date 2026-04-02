3a39be83807c3c7c44e8798e09c06f1413b265cb
aqilknz <khairunnz123@gmail.com>
2026-04-02 22:02:17 +0700

    feat: add Footer

---
+++ src/components/minitask4/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-gray-800 p-5 flex items-center justify-center'>
+        <p className='text-white font-bold text-xl'>Koda Tech Academy - React Js</p>
+    </footer>
+  )
+}
+
+export default Footer
