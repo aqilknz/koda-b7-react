8fc97b5bb87f8785ae1c66aaab1d2645126ca81e
aqilknz <khairunnz123@gmail.com>
2026-04-02 22:10:12 +0700

    feat: add implement routing using react-router

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import Count from './components/minitask1/count.jsx';
-import App from './components/minitask2/App.jsx';
+import Product from './components/minitask2/App.jsx';
 import FetchPokemon from './components/minitask3/fetchPokemon.jsx';
+import Review from './components/minitask4/FormReview.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/pokemon" element={<FetchPokemon />} />
+        <Route path="/" element={<Review />} />
         <Route path="/count" element={<Count />} />
+        <Route path="/app" element={<Product />} />
+        <Route path="/pokemon" element={<FetchPokemon />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
