0a75ab4bf34381188a9844aa4064b65876d981c7
aqilknz <khairunnz123@gmail.com>
2026-04-02 22:04:54 +0700

    feat: add Nav Header and Footer

--- src/components/minitask2/App.jsx
+++ src/components/minitask2/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FormProduct from "./FormProduct";
 import TableProduct from "./TableProduct";
+import Header from "../minitask4/Header.jsx";
+import Footer from "../minitask4/Footer.jsx";
 
 function App() {
   const [products, setProducts] = React.useState([]);
@@ -9,15 +11,19 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 p-8">
-      <div className="w-full mx-auto">
-        <h1 className="text-3xl font-bold text-center mb-8 text-black">
-          Product Management
-        </h1>
-        <FormProduct onAdd={handleAddProduct} />
-        <TableProduct products={products} />
+    <>
+      <Header />
+      <div className="min-h-screen bg-gray-50 p-8">
+        <div className="w-full mx-auto">
+          <h1 className="text-3xl font-bold text-center mb-8 text-black">
+            Product Management
+          </h1>
+          <FormProduct onAdd={handleAddProduct} />
+          <TableProduct products={products} />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
