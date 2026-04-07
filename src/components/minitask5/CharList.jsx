884ccac72568cb87465c56f322e81242ac737987
aqilknz <khairunnz123@gmail.com>
2026-04-07 20:14:23 +0700

    feat: add API URL Character List Rick and Morty

--- src/components/minitask5/CharList.jsx
+++ src/components/minitask5/CharList.jsx
@@ -3,27 +3,30 @@ import Header from '../minitask4/Header.jsx'
 import CardChar from './CardChar.jsx';
 import Footer from '../minitask4/Footer.jsx'
 
+const BASE_URL = "https://rickandmortyapi.com/api/character"
 function CharList() {
   const [characters, setCharacters] = useState([])
 
   useEffect(() => {
-    (async () => {
-      const response = await fetch(import.meta.env.VITE_API_RM)
-      if (!response.ok) {
-        throw new Error(`${response.status} : ${response.statusText}`);
+    const fetchData = async () => {
+      try {
+        
+        const response = await fetch(BASE_URL)
+        if (!response.ok) {
+          throw new Error(`${response.status} : ${response.statusText}`);
+        }
+        const data = await response.json()
+        setCharacters(data.results)
+      } catch (error) {
+        console.error("Gagal mengambil data:", error);
       }
-
-      const data = await response.json()
-      setCharacters(data.results)
-      console.log(data.results)
-
-    })();
-
+    };
+    fetchData();
   }, []);
   return (
     <>
       <Header />
-      <div className='px-10'> 
+      <div className='px-10'>
         <h1 className='font-bold text-3xl text-center my-10 text-pink-600'>Character Rick & Morty</h1>
         <div className="grid grid-cols-1 my-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
           {characters.map((char) => (
@@ -31,7 +34,7 @@ function CharList() {
           ))}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
 
   )
---
+++ src/components/minitask5/DetailChar.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import Header from '../minitask4/Header.jsx';
+import Footer from '../minitask4/Footer.jsx';
+
+function DetailChar() {
+    const [data, setData] = useState(null);
+    const { id, slug } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_API_RM}/${id}`);
+                const data = await response.json();
+
+                if (data.name) {
+                    const correctSlug = data.name.toLowerCase().replace(/\s+/g, '-');
+
+                    if (slug !== correctSlug) {
+                        navigate(`/characters/${id}/${correctSlug}`, { replace: true });
+                    }
+                    setData(data);
+                }
+            } catch (error) {
+                console.error("Error fetching detail:", error);
+            }
+        })();
+    }, [id, slug, navigate]);
+
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-50">
+            <Header />
+
+            <main className=" flex items-center justify-center p-10 min-h-screen">
+                {data ? (
+                    <div className="bg-white border-4 border-pink-600 rounded-3xl overflow-hidden shadow-2xl max-w-4xl w-full flex flex-col md:flex-row">
+
+                        <div className="md:w-1/2 bg-pink-100">
+                            <img
+                                src={data.image}
+                                alt={data.name}
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+
+                        <div className="md:w-1/2 p-8 flex flex-col justify-center gap-6">
+                            <h1 className="text-3xl font-black text-pink-600 uppercase border-b-2 border-pink-100 pb-4">
+                                Detail Character
+                            </h1>
+
+                            <div className="space-y-4">
+                                <h2 className="text-2xl font-bold text-gray-800">
+                                    Name : <span className="text-pink-500">{data.name}</span>
+                                </h2>
+
+                                <h2 className="text-2xl font-bold text-gray-800">
+                                    Status :
+                                    <span className={`ml-2 ${data.status === 'Alive' ? 'text-green-500' : 'text-red-500'}`}>
+                                        {data.status}
+                                    </span>
+                                </h2>
+
+                                <h2 className="text-2xl font-bold text-gray-800">
+                                    Species : <span className="text-gray-600">{data.species}</span>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="text-2xl font-bold text-pink-600 animate-pulse">
+                        Loading Character...
+                    </div>
+                )}
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default DetailChar;
