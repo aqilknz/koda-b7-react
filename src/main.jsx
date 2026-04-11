da9a6e67c2ef50416305d24d6072bf3a9c779e02
aqilknz <khairunnz123@gmail.com>
2026-04-11 22:22:38 +0700

    feat: setup routing and UserProvider in entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import App from './App.jsx'
 import './index.css'
+import UserProvider from './context/userProvider.jsx';
+import Login from './pages/Login.jsx'
 import Count from './components/minitask1/count.jsx';
 import Product from './components/minitask2/App.jsx';
 import FetchPokemon from './components/minitask3/fetchPokemon.jsx';
 import Review from './components/minitask4/FormReview.jsx';
 import FetchChar from './components/minitask5/CharList.jsx'
 import DetailChar from './components/minitask5/DetailChar.jsx';
+import PokemonGallery from './components/useFetch/PokemonGallery.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Review />} />
-        <Route path="/count" element={<Count />} />
-        <Route path="/app" element={<Product />} />
-        <Route path="/pokemon" element={<FetchPokemon />} />
-        <Route path='/characters' element={<FetchChar/>}/>
-        <Route path="/characters/:id/:slug" element={<DetailChar />} />
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={<App />} /> */}
+          <Route path="/" element={<Review />} />
+          <Route path="login" element={<Login />} />
+          <Route path="count" element={<Count />} />
+          <Route path="app" element={<Product />} />
+          <Route path="pokemon" element={<FetchPokemon />} />
+          <Route path='characters'>
+            <Route index element={<FetchChar />} />
+            <Route path=':id'>
+              <Route path=':slug' element={<DetailChar />} />
+            </Route>
+          </Route>
+          <Route path="customhook" element={<PokemonGallery />} />
+
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   </StrictMode>,
 )
