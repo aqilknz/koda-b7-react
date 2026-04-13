20800f99f437030a59ab9ff6f4360696711a9d29
aqilknz <khairunnz123@gmail.com>
2026-04-13 21:38:06 +0700

    feat: add redux provider, persist gate, and main routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
+import { storeProduct, persistor } from "./redux/storeProduct.js";
+import { PersistGate } from "redux-persist/integration/react";
 // import App from './App.jsx'
 import './index.css'
-import ReduxStore from './redux/store.js'
+// import ReduxStore from './redux/store.js'
 import UserProvider from './context/UserProvider.jsx';
 import Login from './pages/Login.jsx'
 import Count from './components/minitask1/count.jsx';
@@ -15,31 +17,36 @@ import FetchChar from './components/minitask5/CharList.jsx'
 import DetailChar from './components/minitask5/DetailChar.jsx';
 import PokemonGallery from './components/useFetch/PokemonGallery.jsx';
 import Survey from './pages/Survey.jsx'
+import ProductRedux from './pages/ProductRedux.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ReduxProvider store={ReduxStore}>
-      <UserProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* <Route path='/' element={<App />} /> */}
-            <Route path="/" element={<Review />} />
-            <Route path="login" element={<Login />} />
-            <Route path="count" element={<Count />} />
-            <Route path="app" element={<Product />} />
-            <Route path="pokemon" element={<FetchPokemon />} />
-            <Route path='characters'>
-              <Route index element={<FetchChar />} />
-              <Route path=':id'>
-                <Route path=':slug' element={<DetailChar />} />
+    {/* <ReduxProvider store={ReduxStore}> */}
+    <ReduxProvider store={storeProduct}>
+      <PersistGate loading={null} persistor={persistor}>
+        <UserProvider>
+          <BrowserRouter>
+            <Routes>
+              {/* <Route path='/' element={<App />} /> */}
+              <Route path="/" element={<Review />} />
+              <Route path="login" element={<Login />} />
+              <Route path="count" element={<Count />} />
+              <Route path="app" element={<Product />} />
+              <Route path="pokemon" element={<FetchPokemon />} />
+              <Route path='characters'>
+                <Route index element={<FetchChar />} />
+                <Route path=':id'>
+                  <Route path=':slug' element={<DetailChar />} />
+                </Route>
               </Route>
-            </Route>
-            <Route path="customhook" element={<PokemonGallery />} />
-            <Route path='survey' element={<Survey />} />
-
-          </Routes>
-        </BrowserRouter>
-      </UserProvider>
+              <Route path="customhook" element={<PokemonGallery />} />
+              <Route path='survey' element={<Survey />} />
+              <Route path="product" element={<ProductRedux />} />
+            </Routes>
+          </BrowserRouter>
+        </UserProvider>
+      </PersistGate>
     </ReduxProvider>
+    {/* </ReduxProvider> */}
   </StrictMode>,
 )
