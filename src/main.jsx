6dbf6bc5ba49af75fe774bc269999f1ebf9be837
aqilknz <khairunnz123@gmail.com>
2026-04-13 16:43:27 +0700

    feat: integrate redux provider into app root

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 // import App from './App.jsx'
 import './index.css'
-import UserProvider from './context/userProvider.jsx';
+import ReduxStore from './redux/store.js'
+import UserProvider from './context/UserProvider.jsx';
 import Login from './pages/Login.jsx'
 import Count from './components/minitask1/count.jsx';
 import Product from './components/minitask2/App.jsx';
@@ -12,28 +14,32 @@ import Review from './components/minitask4/FormReview.jsx';
 import FetchChar from './components/minitask5/CharList.jsx'
 import DetailChar from './components/minitask5/DetailChar.jsx';
 import PokemonGallery from './components/useFetch/PokemonGallery.jsx';
+import Survey from './pages/Survey.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path='/' element={<App />} /> */}
-          <Route path="/" element={<Review />} />
-          <Route path="login" element={<Login />} />
-          <Route path="count" element={<Count />} />
-          <Route path="app" element={<Product />} />
-          <Route path="pokemon" element={<FetchPokemon />} />
-          <Route path='characters'>
-            <Route index element={<FetchChar />} />
-            <Route path=':id'>
-              <Route path=':slug' element={<DetailChar />} />
+    <ReduxProvider store={ReduxStore}>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path='/' element={<App />} /> */}
+            <Route path="/" element={<Review />} />
+            <Route path="login" element={<Login />} />
+            <Route path="count" element={<Count />} />
+            <Route path="app" element={<Product />} />
+            <Route path="pokemon" element={<FetchPokemon />} />
+            <Route path='characters'>
+              <Route index element={<FetchChar />} />
+              <Route path=':id'>
+                <Route path=':slug' element={<DetailChar />} />
+              </Route>
             </Route>
-          </Route>
-          <Route path="customhook" element={<PokemonGallery />} />
+            <Route path="customhook" element={<PokemonGallery />} />
+            <Route path='survey' element={<Survey />} />
 
-        </Routes>
-      </BrowserRouter>
-    </UserProvider>
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
+    </ReduxProvider>
   </StrictMode>,
 )
