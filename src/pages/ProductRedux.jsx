5056d348478bcbc0da8210d7b3dcbd2220604eeb
aqilknz <khairunnz123@gmail.com>
2026-04-13 21:35:07 +0700

    feat: create ProductRedux page for product management with Redux Toolkit

---
+++ src/pages/ProductRedux.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import FormProduct from "../components/ProductRedux/FormProduct.jsx";
+import TableProduct from "../components/ProductRedux/TableProduct.jsx";
+import Header from "../components/minitask4/Header.jsx";
+import Footer from "../components/minitask4/Footer.jsx";
+import { useSelector } from "react-redux";
+
+function ProductRedux() {
+    const products = useSelector((state) => state.products.list);
+    return (
+        <>
+            <Header />
+            <div className="min-h-screen bg-gray-50 p-8">
+                <div className="w-full mx-auto">
+                    <h1 className="text-3xl font-bold text-center mb-8 text-black">
+                        Product Management
+                    </h1>
+                    <FormProduct />
+                    <TableProduct products={products} />
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default ProductRedux
