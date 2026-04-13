3134a333fb4271b7d45771ea8a89c44b4f210f73
aqilknz <khairunnz123@gmail.com>
2026-04-13 22:11:35 +0700

    feat: add product edit/update functionality with Redux

--- src/pages/ProductRedux.jsx
+++ src/pages/ProductRedux.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import FormProduct from "../components/ProductRedux/FormProduct.jsx";
 import TableProduct from "../components/ProductRedux/TableProduct.jsx";
 import Header from "../components/minitask4/Header.jsx";
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 
 function ProductRedux() {
     const products = useSelector((state) => state.products.list);
+    const [editData, setEditData] = useState(null);
     return (
         <>
             <Header />
@@ -15,8 +16,8 @@ function ProductRedux() {
                     <h1 className="text-3xl font-bold text-center mb-8 text-black">
                         Product Management
                     </h1>
-                    <FormProduct />
-                    <TableProduct products={products} />
+                    <FormProduct editData={editData} setEditData={setEditData}/>
+                    <TableProduct products={products} onEdit={setEditData}/>
                 </div>
             </div>
             <Footer />
