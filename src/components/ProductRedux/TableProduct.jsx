5e38bcdd51a5753b09e2925e7073b406d59c62c0
aqilknz <khairunnz123@gmail.com>
2026-04-13 21:31:43 +0700

    feat: implement product list table with delete functionality

---
+++ src/components/ProductRedux/TableProduct.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteProduct } from "../../redux/slice/productSlice";
+
+const TableProduct = ({ products }) => {
+    const dispatch = useDispatch();
+
+    return (
+        <section className="w-3/4 mx-auto mt-12 mb-20">
+            <h2 className="text-2xl font-bold mb-4 text-center">List Product</h2>
+
+            <table className="w-full text-center border">
+                <thead className="border">
+                    <tr className="border">
+                        <th className="border">No</th>
+                        <th className="border">Name</th>
+                        <th className="border">Category</th>
+                        <th className="border">Brand</th>
+                        <th className="border">Stock</th>
+                        <th className="border">Delete</th>
+                    </tr>
+                </thead>
+
+                <tbody className="border">
+                    {products.map((item, index) => (
+                        <tr key={item.id} className="border">
+                            <td className="border">{index + 1}</td>
+                            <td className="border">{item.name}</td>
+                            <td className="border">{item.category}</td>
+                            <td className="border">{item.brand}</td>
+                            <td className="border">{item.count}</td>
+                            <td className="border">
+                                <button onClick={() => dispatch(deleteProduct(item.id))}>
+                                    <img src="/icons/trash.svg" className="w-4 h-4"/>
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </section>
+    );
+};
+
+export default TableProduct;
